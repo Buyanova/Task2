2aae89b838779a3ba97236fc3b80920fe9565937
Buyanova <125025524+Buyanova@users.noreply.github.com>
2023-02-16 20:40:26 +0300

    Add files via upload

---
+++ misc.sql
@@ -0,0 +1,54 @@
+use [Интернет-магазин]
+create table Pokupatel
+(
+Id_pokupatel int Primary key,
+FIO nvarchar(1000) not null,
+Phone nvarchar(15) not null,
+Email nvarchar(50) not null,
+Password nvarchar(50) not null,
+Adres nvarchar(100) not null
+)
+
+create table Haracterystica_tovarov
+(
+Id_kategorii int Primary key, 
+Name_kategorii nvarchar(100) not null,
+Proizvoditelya nvarchar(100) not null,
+strana_proizvoditelya nvarchar(100) not null,
+Brend nvarchar(100) not null,
+Material nvarchar(100) not null,
+Rasmer nvarchar(100) not null
+)
+
+create table Tovar
+(
+Id_tovara int Primary key,
+Name nvarchar(100) not null,
+Id_kategorii int not null,
+Kolichestco int not null,
+Price float not null,
+Image varbinary(max) not null,
+Opisanie_tovara nvarchar(max) not null,
+Foreign key (Id_kategorii) references Haracterystica_tovarov(Id_kategorii) on update cascade on delete cascade
+)
+
+create table Zakaz
+(
+Id_zakaz int Primary key not null,
+Id_pokupatel int not null,
+Date_zakaz Date not null,
+Srok_sborki int not null,
+Dop_info varchar(max) null,
+Adres_dostavki nvarchar(100) null
+Foreign key (Id_pokupatel) references Pokupatel(Id_pokupatel)
+)
+
+create table Soderzhimoe_zakaza
+(
+Id_zakaz int not null,
+Id_tovara int not null,
+Upakovka nvarchar(25) not null,
+Kolichestvo int not null
+Foreign key (Id_zakaz) references Zakaz(Id_zakaz) on update cascade on delete cascade,
+Foreign key (Id_tovara) references Tovar(Id_tovara) on update cascade on delete cascade
+)
